85720bcbbdb4de32e25ee8c1ebfc23348cc70534
godwininyene <godwinhigh2@gmail.com>
2024-10-30 17:39:04 +0100

    Added hosted database on atlas for production

--- utils/email.js
+++ utils/email.js
@@ -15,7 +15,15 @@ module.exports = class Email {
     newTransport(){
         if(process.env.NODE_ENV === 'production'){
             // Using Gmail service
-            return 1;
+            return nodemailer.createTransport({
+                // host:process.env.EMAIL_HOST,
+                // port:process.env.EMAIL_PORT,
+                service:"Gmail",
+                auth:{
+                    user:process.env.EMAIL_USERNAME,
+                    pass:process.env.EMAIL_PASSWORD
+                }
+            })
         }
 
         return nodemailer.createTransport({
