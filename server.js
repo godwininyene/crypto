44829ee7fd0e5438a2ad5bcb132bb4845d901064
godwininyene <godwinhigh2@gmail.com>
2024-10-31 05:30:29 +0100

    Fix database connection string issue

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path:'./config.env'});
-const MongoClient = require('mongodb').MongoClient;
-const Plan = require('./models/plan')
 
 process.on('uncaughtException', err=>{
     console.log("uncaughtException! shutting down...")
@@ -11,41 +9,18 @@ process.on('uncaughtException', err=>{
 })
 
 const app = require('./app');
-// mongoose.connect(process.env.DB_LOCAL, {
-//     useNewUrlParser:true,
-//     useCreateIndex:true,
-//     useFindAndModify:false,
-//     useUnifiedTopology:true
-// }).then(()=>console.log('Database connected successfully'))
-
 
 let DB;
+
 if (process.env.NODE_ENV === 'development') {
     DB = process.env.DB_LOCAL;
 } else if (process.env.NODE_ENV === 'production') {
-    // DB = process.env.DB_LOCAL;
     DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 }
 
 
-mongoose.connect(DB, {
-    useNewUrlParser: false,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-}).then(async () => {
-    console.log('DB connection successfully')
-    const plans = await Plan.find();
-    console.log(plans)
-})
+mongoose.connect(DB).then(() => {console.log('DB connection successfully')})
 
-// const conn = mongoose.createConnection(DB, {useNewUrlParser:true})
-
-// const client = new MongoClient(DB);
-
-// client.connect()
-// .then(()=> console.log("Connected successfully"))
-// .catch(err => console.log(err.name, err.message))
 
 const port = process.env.PORT || 3000
 const server = app.listen(port, ()=>{
--- utils/email.js
+++ utils/email.js
@@ -13,18 +13,18 @@ module.exports = class Email {
     }
 
     newTransport(){
-        if(process.env.NODE_ENV === 'production'){
-            // Using Gmail service
-            return nodemailer.createTransport({
-                // host:process.env.EMAIL_HOST,
-                // port:process.env.EMAIL_PORT,
-                service:"Gmail",
-                auth:{
-                    user:process.env.EMAIL_USERNAME,
-                    pass:process.env.EMAIL_PASSWORD
-                }
-            })
-        }
+        // if(process.env.NODE_ENV === 'production'){
+        //     // Using Gmail service
+        //     return nodemailer.createTransport({
+        //         host:process.env.EMAIL_HOST,
+        //         port:process.env.EMAIL_PORT,
+        //         service:"Gmail",
+        //         auth:{
+        //             user:process.env.EMAIL_USERNAME,
+        //             pass:process.env.EMAIL_PASSWORD
+        //         }
+        //     })
+        // }
 
         return nodemailer.createTransport({
             host:process.env.EMAIL_HOST,
